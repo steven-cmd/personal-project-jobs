ca62039fe18a2ecf906978c8061d2ec56a0944cb
Steven Clark <stevenclark@steb.localdomain>
2021-05-21 16:01:08 -0600

    added user controller

---
+++ server/controllers/user.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  register: async (req, res) => {
+    const db = req.app.get("db");
+    const { email, password, location } = req.body;
+    const [isUser] = await db.user.get_user_by_email(email);
+    if (isUser) {
+      return res.status(409).send("email already exists");
+    }
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(password, salt);
+    const [user] = await db.user.create_user(email, hash, location);
+    delete user.password;
+    req.session.user = user;
+    return res.status(200).send(req.session.user);
+  },
+  login: async (req, res) => {
+    const db = req.app.get("db");
+    const { email, password } = req.body;
+    const [user] = await db.user.get_user_by_email(email);
+    if (!user) {
+      return res.status(401).send("no such email");
+    }
+    const isAuthenticated = bcrypt.compareSync(password, user.password);
+    if (!isAuthenticated) {
+      return res.status(403).send("wrong password");
+    }
+    delete user.password;
+    req.session.user = user;
+    return res.status(200).send(req.session.user);
+  },
+  getUser: (req, res) => {
+    if (!req.session.user) {
+      return res.status(404).send("no such user");
+    }
+    return res.status(200).send(req.session.user);
+  },
+  logout: (req, res) => {
+    req.session.destroy();
+    return res.sendStatus(200);
+  },
+};
--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const massive = require("massive");
 const session = require("express-session");
 const express = require("express");
 const { CONNECTION_STRING, SESSION_SECRET, SERVER_PORT } = process.env;
-const authCtrl = require("./controllers/authController");
-const skillCtrl = require("./controllers/skillController");
+const userCtrl = require("./controllers/user");
+const skillCtrl = require("./controllers/skill");
 const app = express();
 
 app.use(express.json());
