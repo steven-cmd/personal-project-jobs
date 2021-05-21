121d082a0a324cc85fc9aca674794775ab3c71f1
Steven Clark <stevenclark@steb.localdomain>
2021-05-21 15:17:51 -0600

    first commit

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+create table users 
+(id serial primary key,
+email varchar not null, 
+password varchar not null,
+location varchar);
+
+create table skills
+(id serial primary key,
+skill varchar not null);
+
+create table userskills
+(id serial primary key,
+user_id integer references users(id),
+skill_id integer references skills(id));
