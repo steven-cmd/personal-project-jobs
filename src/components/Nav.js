b32b2a41a90567b9432b3058314c5bfb46aea5ed
Steven Clark <stevenclark@steb.localdomain>
2021-05-25 14:01:56 -0600

    authentication front end improvements

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../redux/userReducer";
+import axios from "axios";
+import { useEffect } from "react";
 
-const Nav = () => {
+const Nav = (props) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const { user } = useSelector((state) => state.userReducer);
+
+  const handleLogout = () => {
+    axios
+      .get("/user/logout")
+      .then((res) => {
+        dispatch(logout(res.data));
+        history.push("/");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
       <Link to="/">
@@ -8,7 +27,14 @@ const Nav = () => {
       </Link>
       <Link to="/">Browse</Link>
       <Link to="/about">About</Link>
-      <Link to="/Auth">Pro</Link>
+      {user ? <Link to="/pro">Pro</Link> : <Link to="/Auth">Pro</Link>}
+      {user ? (
+        <button onClick={handleLogout}>Logout</button>
+      ) : (
+        <Link to="/Auth">
+          <button>Sign In</button>
+        </Link>
+      )}
     </div>
   );
 };
