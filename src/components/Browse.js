52e2f66f8edf338f41326768c1196510c1571131
Steven Clark <stevenclark@steb.localdomain>
2021-05-26 17:02:12 -0600

    trying out tfidf

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,16 +1,24 @@
 import { useSelector } from "react-redux";
+const natural = require("natural");
+const TfIdf = natural.TfIdf;
+const tfidf = new TfIdf();
 
 const Browse = () => {
   const { jobs } = useSelector((store) => store.jobReducer);
-  console.log(jobs);
-  return (
-    <div>
-      Look at browse
-      {jobs.map((job) => {
-        return <p>{job.name}</p>;
-      })}
-    </div>
-  );
+
+  for (const job of jobs) {
+    tfidf.addDocument(job.name[0]);
+  }
+
+  const titleTfidf = new Set();
+
+  for (let i = 0; i < jobs.length; i++) {
+    tfidf.listTerms(i).forEach(function (item) {
+      console.log(item.term + ": " + item.tfidf);
+    });
+  }
+
+  return <div>Look at browse</div>;
 };
 
 export default Browse;
